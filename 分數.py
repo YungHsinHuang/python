71e59fb02ac71a6885e39b0547c25f91ef610758
YungHsinHuang <88917869+YungHsinHuang@users.noreply.github.com>
2021-08-21 12:28:00 +0800

    Add files via upload

---
+++ 分數.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 11:27:50 2021
+
+@author: user
+"""
+
+math=98
+english=95
+if english>=90 and math>=90:
+    print("有獎品!!!")
+if english<60 and math<60:
+    print("要處罰!!!")
+if engilsh<60 or math<60:
+    print("再加油!!!")
+    
