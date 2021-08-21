a31a6a870b5e5f433e0bdaf27d8de5474335374a
YungHsinHuang <88917869+YungHsinHuang@users.noreply.github.com>
2021-08-21 10:26:00 +0800

    園面積周長

---
+++ 園.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 10:24:04 2021
+
+@author: user
+"""
+
+半徑=float(input("請輸入園半徑"))
+圓周=2*3.14*半徑
+園面積=3.14*(半徑**2)
+print("圓周=",圓周)
+print("園面積=",園面積)
